77d99a8d7f5df71bec16339c1fc168ed16bf5e84
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-19 00:18:54 -0500

    implemented without function

---
+++ without.js
@@ -0,0 +1,44 @@
+const assertArraysEqual = (arr1, arr2) => {
+
+  const message = eqArrays(arr1, arr2)
+
+    ? "✅ Arrays are equal."
+    : "❌ Arrays are not equal."
+
+  console.log(message);
+};
+
+const eqArrays = (arr1, arr2) => {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
+const without = (source, itemsToRemove) => {
+
+  const result = source.filter(item => !itemsToRemove.includes(item));
+  return result;
+
+};
+
+const words = ["hello", "world", "lighthouse"];
+
+const result1 = without([1, 2, 3], [1]);
+assertArraysEqual(result1, [2, 3]);
+
+const result2 = without(["1", "2", "3"], [1, 2, "3"]);
+assertArraysEqual(result2, ["1", "2"]);
+
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
