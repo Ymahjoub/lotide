508fd13fbde9232f1ad70c16349303635f25be80
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-18 23:48:10 -0500

    implemented true or false function

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log('✅ Assestion Passed: ${actual} === ${expected}');
+  } else {
+    console.log('❌ Assestion Failed: ${actual} !== ${expected}');
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);// => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false 
+
