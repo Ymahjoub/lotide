00ff452f7e031823a9530af3989dbab9687ee0f8
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-19 17:58:37 -0500

    made change to for const function

--- countOnly.js
+++ countOnly.js
@@ -1,29 +1,33 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-}; 
+};
 
-const countOnly = function(allItems, itemsToCount) {
-  const results = {}; 
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
 
   for (const item of allItems) {
-   if (results[item]) {
-    results[item] += 1;
-   } else { 
-    results[item] = 1;
-  }
-} 
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
 
-for (const item in allItems) {
-  if (itemsToCount[item]) {
-    console.log(item, results[item]);
-  }
-}
+    }
 
-return results;
+  }
+
+  for (const item in allItems) {
+    if (itemsToCount[item]) {
+      console.log(item, results[item]);
+    }
+  }
+
+  return results;
 
 };
 
