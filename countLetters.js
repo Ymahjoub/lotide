9103fa28bd45525a36347b0b0c3a0c7a8305c4b5
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-19 18:10:34 -0500

    made count letters function

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const letterCount = {};
+
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+
+      if(letterCount[letter]) {
+        letterCount[letter]++;
+
+      } else {
+        letterCount[letter] = 1;
+
+    }
+  }
+} 
+
+return letterCount; 
+
+}; 
+
+const result = countLetters('LHL');
+console.log(result);
