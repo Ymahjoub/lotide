ae9d361d209a288617667220d31b1f63632daa99
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-20 02:45:14 -0500

    made findKeysByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}; 
+
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+}; 
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70 Show"), undefined);
+assertEqual(findKeyByValue({}, "Some Value"), undefined);
+assertEqual(findKeyByValue({ key1: "value1", key2: "value2"}, "value1"), "key1");
+
+
