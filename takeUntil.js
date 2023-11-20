6bddc141f8e54d915666e22c81372d65bee05c64
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-20 15:36:10 -0500

    made takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,45 @@
+const eqArrays = (arr1, arr2) => {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+
+}; 
+
+const assertArraysEqual = function(actual, expected) {
+  if(eqArrays(actual, expected)) {
+    console.log("Arrays are equal!");
+  } else {
+    console.log("Arrays are not qual!")
+  }
+}; 
+
+const takeUntil = function(array , callback) {
+  const result = [];
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    }
+    result.push(item);
+  }
+  return result;
+};
+
+//Test Ex 
+const data1 = [1, 2, 5, 7, -1, 2, 4, 5];
+const result1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(result1, [1, ,2, 5, 7, 2]);
+
+console.log('---'); 
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const result2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(result2, ["I've", "been", "to", "Hollywood"])
