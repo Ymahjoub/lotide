8bdbb5d214cc1052743b6ed0f7791ad6e16021fe
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-20 15:49:25 -0500

    created findKey function

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callabck) {
+  for (const key in object) {
+    if (callabck(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const restaurants = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 } 
+}; 
+
+assertEqual(findKey(restaurants, x => x.stars === 2), "noma");
+assertEqual(findKey(restaurants, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(restaurants, x => x.stars === 4), undefined);
