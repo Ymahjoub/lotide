83ba3f5adb23d1af3cbbfdde1c582c14ef7e0985
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-22 23:08:03 -0500

    used ES6 in index file

--- index.js
+++ index.js
@@ -10,25 +10,24 @@ const countOnly = require('./countOnly');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  eqArrays: eqArrays,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters, 
-  countOnly: countOnly, 
-  eqObjects: eqObjects, 
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions, 
-  takeUntil: takeUntil, 
-  without: without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  eqArrays,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters, 
+  countOnly, 
+  eqObjects, 
+  findKey,
+  findKeyByValue,
+  letterPositions, 
+  takeUntil,
+  without
 };
