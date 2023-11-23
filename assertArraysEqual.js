5c41409c8c69c6b52a05111a4a4bb6727bc4d466
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-22 19:30:31 -0500

    refactored assertArraysEqual and added test to assertArraysEqualTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,5 @@
-const eqArrays = (arr1, arr2) => {
 
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = (arr1, arr2) => {
 
@@ -24,11 +11,7 @@ const assertArraysEqual = (arr1, arr2) => {
   console.log(message);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
+module.exports = assertArraysEqual
 
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); 
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
 
